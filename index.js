03b42d6a35f68dfa1698a236d8ed724f8bcc3234
TomasGLLOK <tomasgll2996@gmail.com>
2024-09-25 11:23:21 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,87 @@
+
+const express = require('express')
+const app = express()
+
+app.use(express.json())
+
+const generateId = () => {
+    const maxId = note.length > 0
+    ? Math.max(...notes.map(n => n.id))
+    : 0
+
+    return maxId + 1
+}
+
+let notes = [
+    {
+        id: 1,
+        content: "HTML is easy",
+        important: true
+    },
+    {
+        id: 2,
+        content: "Browser can execute only JavaScript",
+        important: false
+    },
+    {
+        id: 3,
+        content: "GET and POST are the most important methods of HTTP protocol",
+        important: true
+    }
+]
+
+app.get('/', (request, response) => {
+    response.send('<h1>Hello World!</h1>')
+})
+
+app.get('/api/notes', (request, response) => {
+    response.json(notes)
+})
+
+app.get('/api/notes/:id', (request, response) => {
+    const id = Number(request.params.id)
+    console.log(id)
+    const note = notes.find(note => note.id === id)
+    
+    if (note) {
+        response.json(note)
+    } else {
+        response.status(404).end()
+    }
+
+})
+
+app.delete('/api/notes/:id', (request, response) => {
+    const id = Number(request.params.id)
+    notes = notes.filter(note => note.id !== id)
+
+    response.status(204).end()
+})
+
+app.post('/api/notes', (request, response) => {
+    const body = request.body
+
+    if (!body.content) {
+        return response.status(404).json({
+            error: 'content missing'
+        })
+    }
+
+    const note = {
+        content: body.content,
+        important: Boolean(body.important) || false,
+        id: generateId()
+    }
+
+    notes = notes.concat(note)
+    response.json(note)
+})
+
+/**
+ * Finalmente, se enlaza el servidor http a la variable app para "escuchar" las solicitudes
+ * http enviadas al puerto 3001
+ */
+const PORT = 3001
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
